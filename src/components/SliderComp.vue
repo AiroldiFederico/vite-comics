3c87b96cc63411ac5f60acf8fcc46850ec5eb4d8
Federico Airoldi <airoldibusiness@gmail.com>
2023-05-05 01:06:19 +0200

    FASE 8 - Creo un componente figlio con props e ciclo le immagini nel core

---
+++ src/components/SliderComp.vue
@@ -0,0 +1,52 @@
+<script>
+
+    export default{
+        name: "SliderComp",
+
+        props: {
+            thumb: String,
+            price: String,
+            series: String,
+            type: String,
+        },
+
+        components: {
+
+        },
+        data(){
+            return{
+               
+
+
+            }
+        },
+        methods: {
+            
+        },
+    }
+    
+</script>
+
+
+
+
+<template>
+    <div>
+        <img :src="thumb" alt="">
+        <!-- <img :src="item.thumb" alt="" v-for="(item, index) in sliderchild" :key="index"> -->
+    </div>
+</template>
+
+
+
+
+
+<style lang="scss" scoped>
+
+div {
+    
+    img {
+        width: 5rem;
+    }
+}
+</style>
